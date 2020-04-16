c59a1c51fb8e8e177ffa68f934031c6e5acf41ba
Carsten Sandtner <sandtner@mediaman.de>
2020-04-16 20:34:46 +0200

    New directory structure

--- scripts/clean_and_save.py
+++ scripts/clean_and_save.py
@@ -30,13 +30,21 @@ def clean_and_save_worldwide(df):
     df_cases = df.groupby(['Country_Region'], as_index=False).sum()
     df_cases.to_csv('Total_cases_wordlwide.csv')
 
+def clean_and_save_german_states(df):
+    df.columns = ['Bundesland', 'Anzahl', 'diff', 'Pro_Tsd', 'Gestorben']
+    df.drop(columns=['diff'], index=[16], inplace=True)
+    df.to_csv('cases_germany_states.csv')
 
 if __name__ == '__main__':
 
     df_countries = pd.read_csv('../time_series_covid19_confirmed_global.csv')
     df_world_wide = pd.read_csv('../04-13-2020.csv')
+    df_rki = pd.read_html('../Fallzahlen.html', decimal=',', thousands='.')
+    df_de_states = df_rki[0]
 
     clean_and_save_country('Germany', df_countries)
     clean_and_save_worldwide(df_world_wide)
+    clean_and_save_german_states(df_de_states)
+    
 
     exit(0)
