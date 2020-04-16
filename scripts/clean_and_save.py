772bbf41a81fb0ed235778b7856163e30cb9a308
Carsten Sandtner <sandtner@mediaman.de>
2020-04-16 21:00:16 +0200

    Documented scripts.

--- scripts/clean_and_save.py
+++ scripts/clean_and_save.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 def clean_and_save_country(country_name, df):
+    """ Clean country data and save cleaned CSV
+    
+    Arguments:
+        country_name {String} -- Name of the Country to save
+        df {Dataframe} -- Pandas Dataframe
+    """
     drop_columns = ['Lat', 
                     'Long', 
                     'Province/State']
@@ -15,9 +21,14 @@ def clean_and_save_country(country_name, df):
     country.reset_index(level=0, inplace=True)
     country['date'] = pd.to_datetime(country['index'])
     country.drop(columns = ['index'], inplace=True)
-    country.to_csv(country_name + '_timeseries.csv')
+    country.to_csv('../data' + country_name + '_timeseries.csv')
 
 def clean_and_save_worldwide(df):
+    """Clean Worldwide infections and save to CSV
+    
+    Arguments:
+        df {Dataframe} -- Pandas Dataframe to be cleaned.
+    """
     drop_columns = ['FIPS',
                     'Lat', 
                     'Long_', 
@@ -28,23 +39,27 @@ def clean_and_save_worldwide(df):
     df.drop(columns=drop_columns, inplace=True)
 
     df_cases = df.groupby(['Country_Region'], as_index=False).sum()
-    df_cases.to_csv('Total_cases_wordlwide.csv')
+    df_cases.to_csv('../data/Total_cases_wordlwide.csv')
 
 def clean_and_save_german_states(df):
+    """Clean Data for fedaral states in germany. Save as CSV
+    
+    Arguments:
+        df {Dataframe} -- PAndas Dataframe to be cleaned.
+    """
     df.columns = ['Bundesland', 'Anzahl', 'diff', 'Pro_Tsd', 'Gestorben']
     df.drop(columns=['diff'], index=[16], inplace=True)
-    df.to_csv('cases_germany_states.csv')
+    df.to_csv('../data/cases_germany_states.csv')
+
 
 if __name__ == '__main__':
-
-    df_countries = pd.read_csv('../time_series_covid19_confirmed_global.csv')
-    df_world_wide = pd.read_csv('../04-13-2020.csv')
-    df_rki = pd.read_html('../Fallzahlen.html', decimal=',', thousands='.')
+    df_countries = pd.read_csv('../original_data/time_series_covid19_confirmed_global.csv')
+    df_world_wide = pd.read_csv('../original_data/04-13-2020.csv')
+    df_rki = pd.read_html('../original_data/Fallzahlen.html', decimal=',', thousands='.')
     df_de_states = df_rki[0]
 
     clean_and_save_country('Germany', df_countries)
     clean_and_save_worldwide(df_world_wide)
     clean_and_save_german_states(df_de_states)
     
-
     exit(0)
